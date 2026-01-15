396273ca4e91ead6ddc3fe7c9a47859f3b3a6235
SantiGVadone <santiagovadone36@gmail.com>
2026-01-15 17:42:47 -0300

    Fin de la 3era Clase

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,7 +1,7 @@
 const express = require('express') // usamos el require porque estamos usando common js
 const movies = require('./movies')
 const crypto = require('node:crypto')
-
+const {validateMovie,validatePartialMovie} = require('./schemes/movies.js')
 
 const app = express()
 app.use(express.json())
@@ -13,6 +13,10 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/movies',(req,res)=>{
+    res.header('Access-Control-Allow-Origin','*') // podes poner solo los origenes que aceptas ejemplo http://localhost:8080,
+                                                    //o podes poner '*' y de esa forma le das entrada a todos los dominios
+//esto funciona para los metodos como put get pathc, no para los metodos complejos como el delete
+
     const {genre} = req.query
     if(genre){
         const filteredMovies = movies.filter(
@@ -34,26 +38,16 @@ app.get('/movies/:id',(req,res)=>{ //esto es crear un end point, osea que estoy
 }) //esto se conoce como path-to-regexp 
 
 app.post('/movies',(req,res)=>{
-    const {
-        title,
-        director,
-        year,
-        rate,
-        poster,
-        genre
-    } = req.body
+    const result = validateMovie(req.body)
+    if(!result.success){ //esto seria lo mismo que decir result.error
+        return res.status(400).json({error: JSON.parse(result.error.message)})
+    }
 
     const newMovie = {
         id: crypto.randomUUID(), // crea un UUID (IDENTIFICADOR UNICO UNIVERSAL) version 4
-        title,
-        director,
-        year,
-        rate,
-        poster,
-        genre
+        ...result.data
     }
         
-
     //esto no seria rest porque estamos guardando
     //el estado de la aplicacion en memoria
     movies.push(newMovie)
@@ -62,6 +56,33 @@ app.post('/movies',(req,res)=>{
 
 })
 
+app.patch('/movies/:id',(req,res)=>{
+
+    
+    const result = validatePartialMovie(req.body)
+
+    if(!result.success){
+        return res.status(400).json({error: JSON.parse(result.error.message)}) //algunas de las actualizaciones no son validas
+    }
+
+    const {id} = req.params
+
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if(movieIndex === -1){
+        return res.status(404).json({message:'Movie not found'}) //esta en -1 porque no se encontro la pelicula
+    }  
+
+    const updateMovie = { //esto va a ser igual a todo lo que tengamos de la peli + todo lo que tengamos de la data nueva
+        ...movies[movieIndex], //agarra todos los datos viejos
+        ...result.data         //agarra todos los datos nuevos, y los pone encima
+    }
+    
+    movies[movieIndex] = updateMovie //deja de estar en el aire y lo remplaza
+
+    return res.json(updateMovie)
+
+})
 
 const PORT = process.env.PORT || 1234
 
